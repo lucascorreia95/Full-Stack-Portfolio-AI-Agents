5e40721b18a643eeebf6259e50fb217819b4b5c5
Lucas Correia <lucas_p_correia_cadmus@whirlpool.com>
2025-03-19 16:27:10 -0300

    feat: dia 2 backend

--- backend/src/app.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import { test } from '@core';
-
-@Injectable()
-export class AppService {
-  getHello(): string {
-    return 'Hello World!' + test;
-  }
-}
